c2c0eaa5f1fdf99630a61b444d87bb6e258efe7c
munzli <manuel@monostream.com>
2017-10-18 16:32:28 +0200

    possibility to use proxy for freshclam

--- docker/readiness.sh
+++ docker/readiness.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # freshclam
-#if freshclam | grep -q 'bytecode.* is up to date'; then
-#    echo "freshclam running successfully"
-#else
-#    echo "freshclam not running"
-#    exit 1
-#fi
+if freshclam | grep -q 'bytecode.* is up to date'; then
+    echo "freshclam running successfully"
+else
+    echo "freshclam not running"
+    exit 1
+fi
 
 # clamd
 if clamdscan eicar.com | grep -q 'Infected files: 1'; then
--- docker/start.sh
+++ docker/start.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+
+# check proxy environment and replace in freshclam.conf
+if [ $http_proxy ]; then
+    host=$(echo $http_proxy | sed -n "s/^https\?:\/\/\(.*\):.*\?\/\?/\1/p")
+    port=$(echo $http_proxy | sed -n "s/^https\?:\/\/.*:\(.*\?\)\/\?/\1/p")
+
+    sed -i "s/#HTTPProxyServer myproxy.com/HTTPProxyServer $host/g" /etc/clamav/freshclam.conf
+    sed -i "s/#HTTPProxyPort 1234/HTTPProxyPort $port/g" /etc/clamav/freshclam.conf
+fi
+
 freshclam -d &
 clamd
