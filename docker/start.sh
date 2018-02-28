a700a47e6f5998337e4ad9899485e1c61df4a7d4
munzli <manuel@monostream.com>
2018-02-28 16:00:53 +0100

    make sure config files are writable as non-privileged user

--- docker/start.sh
+++ docker/start.sh
@@ -5,9 +5,9 @@ if [ $http_proxy ]; then
     host=$(echo $http_proxy | sed -n "s/^https\?:\/\/\(.*\):.*\?\/\?/\1/p")
     port=$(echo $http_proxy | sed -n "s/^https\?:\/\/.*:\(.*\?\)\/\?/\1/p")
 
-    sed -i "s/#HTTPProxyServer myproxy.com/HTTPProxyServer $host/g" /etc/clamav/freshclam.conf
-    sed -i "s/#HTTPProxyPort 1234/HTTPProxyPort $port/g" /etc/clamav/freshclam.conf
+    sed -i "s/#HTTPProxyServer myproxy.com/HTTPProxyServer $host/g" /app/conf/freshclam.conf
+    sed -i "s/#HTTPProxyPort 1234/HTTPProxyPort $port/g" /app/conf/freshclam.conf
 fi
 
-freshclam -d --no-dns &
-clamd
+freshclam -d --no-dns --config-file=/app/conf/freshclam.conf &
+clamd  --config-file=/app/conf/clamd.conf
