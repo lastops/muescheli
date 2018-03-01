8bd271570073200aad96cdb38042ba7e3c416ca7
Dominik Hahn <dominik@monostream.com>
2018-03-01 12:25:00 +0100

    Fix liveness checks

--- docker/liveness.sh
+++ docker/liveness.sh
@@ -6,9 +6,8 @@ path=$(cd -P -- "$(dirname -- "$0")" && printf '%s\n' "$(pwd -P)")
 # clamd
 if clamdscan ${path}/eicar.com | grep -q 'Infected files: 1'; then
     echo "clamd running successfully"
+    exit 0
 else
     echo "clamd not running"
     exit 1
 fi
-
-exit 0
--- docker/readiness.sh
+++ docker/readiness.sh
@@ -3,9 +3,8 @@
 # freshclam
 if freshclam | grep -Eq 'bytecode.* is up to date|bytecode.* updated'; then
     echo "freshclam running successfully"
+    exit 0
 else
     echo "freshclam not running"
     exit 1
 fi
-
-exit 0
